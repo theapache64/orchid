8be33124725a37058b59cef9701e0391ffa74eb2
theapache64 <theapache64@gmail.com>
2019-01-17 10:14:53 +0530

    Added new splash activity

---
+++ app/src/main/java/com/tp/orchid/ui/activities/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.tp.orchid.ui.activities.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tp.orchid.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
