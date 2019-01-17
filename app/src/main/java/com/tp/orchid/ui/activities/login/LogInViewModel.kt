2405b31b5e70045c7ceb84eac9401423ae84742d
theapache64 <theapache64@gmail.com>
2019-01-17 22:36:28 +0530

    login flow works (partial)

--- app/src/main/java/com/tp/orchid/ui/activities/login/LogInViewModel.kt
+++ app/src/main/java/com/tp/orchid/ui/activities/login/LogInViewModel.kt
@@ -1,17 +1,21 @@
 package com.tp.orchid.ui.activities.login
 
-import androidx.arch.core.util.Function
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.lifecycle.*
+import com.google.gson.Gson
 import com.tp.orchid.data.remote.login.LogInRequest
 import com.tp.orchid.data.remote.login.LogInResponse
 import com.tp.orchid.data.repositories.LogInRepository
 import com.tp.orchid.utils.Resource
+import com.tp.orchid.utils.extensions.info
 import javax.inject.Inject
 
-class LogInViewModel @Inject constructor(val logInRepository: LogInRepository) : ViewModel() {
+class LogInViewModel @Inject constructor(
+        val logInRepository: LogInRepository,
+        val sharedPreferences: SharedPreferences,
+        val gson: Gson
+) : ViewModel() {
 
     var username: String = ""
 
@@ -21,11 +25,29 @@ class LogInViewModel @Inject constructor(val logInRepository: LogInRepository) :
         logInRepository.login(it)
     }
 
+    private val loginMerger = MediatorLiveData<Resource<LogInResponse>>()
+
+    init {
+        loginMerger.addSource(loginResponseLiveData) {
+            if (it.status == Resource.Status.SUCCESS) {
+                saveUser(it.data!!.data.user)
+            }
+            loginMerger.value = it
+        }
+    }
+
+    private fun saveUser(user: LogInResponse.User) {
+        sharedPreferences.edit {
+            putString(LogInResponse.User.KEY, gson.toJson(user))
+            commit()
+        }
+    }
+
     fun onLogInPressed() {
         loginRequestLiveData.value = LogInRequest(username, password)
     }
 
-    fun getLogInLiveData(): LiveData<Resource<LogInResponse>> = loginResponseLiveData
+    fun getLogInLiveData(): LiveData<Resource<LogInResponse>> = loginMerger
 
 
 }
