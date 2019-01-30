57fc1dababf21ae9b9e80e3915b3e5f508510a27
theapache64 <theapache64@gmail.com>
2019-01-30 11:12:51 +0530

    Added launcher icon and icons for glide's error and placeholder

--- app/src/main/java/com/tp/orchid/ui/adapters/bindingadapters/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/tp/orchid/ui/adapters/bindingadapters/ImageViewBindingAdapter.kt
@@ -4,11 +4,15 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.tp.orchid.R
 
 @BindingAdapter("imageUrl")
 fun loadImage(imageView: ImageView, url: String) {
 
     val requestOption = RequestOptions()
+        .centerCrop()
+        .error(R.drawable.ic_error_outline_primary_dark_100dp)
+        .placeholder(R.drawable.ic_video_camera_primary_dark_100dp)
 
     Glide.with(imageView.context)
         .load(url)
